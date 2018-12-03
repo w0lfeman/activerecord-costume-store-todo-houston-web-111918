5854accb9f1360bbf547010ec8e9833bbd37fe51
David Wolfe <dwolfe1987@gmail.com>
2018-12-03 15:56:48 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,3 +1,5 @@
+class CreateCostumes < ActiveRecord::Migration [5.1]
+
 # Create a class and inherit from ActiveRecord::Migration
 
 # By convention, the class name should match the part of the
@@ -7,3 +9,13 @@
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and 
 # value types according to the spec
+
+def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :genre 
+      t.string :size 
+      t.text :image_url
+    end
+  end
+end
